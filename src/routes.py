3796c0035ad702c7feb14695987e39244d8076a6
Tegar <tegaranugroho@gmail.com>
2023-06-12 17:02:17 +0700

    add line spacing

--- src/routes.py
+++ src/routes.py
@@ -155,7 +155,7 @@ def print_receipt():
         device.set(align='center', text_type='B')
         device.text("P&C POS App\n")
         device.set(align='center', text_type='NORMAL')
-        device.text("\n-------------------------------\n")
+        device.text("\n-----------------------------------------\n")
         
         for item in receipt_data['items']:
             name = item['name']
@@ -167,7 +167,7 @@ def print_receipt():
             line = f"{name}{' ' * space_count}{price}"
             device.text(line + '\n')
         
-        device.text("-------------------------------\n\n")
+        device.text("-----------------------------------------\n\n")
         device.line_spacing()
         device.line_spacing()
         device.barcode("123456", "CODE39")
