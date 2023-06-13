10c90339fa7e9c96b266a6c1e0cfb8bd072b0a71
Tegar <tegaranugroho@gmail.com>
2023-06-13 11:10:26 +0700

    add line spacing

--- src/routes.py
+++ src/routes.py
@@ -171,7 +171,7 @@ def print_receipt():
             # Calculate the space counts
             number_space_count = len(number)
             name_space_count = 18 - len(name)  # Adjust the space count as needed
-            product_id_space_count = len(product_id) - 2
+            product_id_space_count = len(product_id) - 2.5
             
             title_line = f"{column_titles[0]}{' ' * (number_space_count - 1)}" \
                    f"{column_titles[1]}{' ' * (name_space_count + 5)}" \
