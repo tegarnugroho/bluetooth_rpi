1859b3c6ea97ed49ab5ab1a52bc55651f6a85248
Tegar <tegaranugroho@gmail.com>
2023-06-13 15:26:43 +0700

    add line spacing

--- src/routes.py
+++ src/routes.py
@@ -202,7 +202,7 @@ def print_receipt():
         
         border_line(device, 48)    
         device.set(text_type='B', font='A', width=2, height=2)  # Set larger size and bold format
-        device.text(f"Gesamtbetrag {space(4)}200\n")
+        device.text(f"Gesamtbetrag {space(8)}200\n")
         device.set(text_type='NORMAL', font='A', width=1, height=1) 
         
         border_line(device, 48)
